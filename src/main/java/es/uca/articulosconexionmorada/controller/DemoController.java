28d1a3f20e645b05afe3d538e09949a38681f2fc
saikora11 <pedro.garciaromera@alum.uca.es>
2023-01-17 16:38:30 +0100

    Initial Commit

---
+++ src/main/java/es/uca/articulosconexionmorada/controller/DemoController.java
@@ -0,0 +1,24 @@
+package es.uca.articulosconexionmorada.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import es.uca.articulosconexionmorada.bean.Persona;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping(value = "/person", method = RequestMethod.GET)
+    public Persona obtenerPerson(HttpServletRequest servletRequest) {
+
+    	Persona user = new Persona();
+        user.setDocumento("01234");
+        user.setNombre("Peter");
+        user.setApellidos("Parker");
+
+        return user;
+    }
+}
