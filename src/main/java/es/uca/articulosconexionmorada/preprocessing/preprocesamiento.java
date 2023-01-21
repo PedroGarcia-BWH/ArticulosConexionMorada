bf622c0d5ce1bacc6d181fc0f360c793520cf4a9
Pedro Jose Garcia Romera <pedro.garciaromera@alum.uca.es>
2023-01-21 21:51:15 +0100

    bug: acentos arreglado

--- src/main/java/es/uca/articulosconexionmorada/preprocessing/preprocesamiento.java
+++ src/main/java/es/uca/articulosconexionmorada/preprocessing/preprocesamiento.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Locale;
+import org.apache.commons.lang3.StringUtils;
 
 public class preprocesamiento {
     private gestorFiltro gestorCaracters = new gestorFiltro();
@@ -26,7 +27,8 @@ public class preprocesamiento {
 
         //preprocesamos
             // convertimos todo el texto a minúsculas
-            sTexto = sTexto.toLowerCase(Locale.ENGLISH);
+            sTexto = sTexto.toLowerCase(Locale.ROOT);
+            sTexto = StringUtils.stripAccents(sTexto);
             //aplicamos el filtro
             sTexto = gestorCaracters.apply(sTexto);
             //System.out.println(sTexto);
@@ -38,15 +40,18 @@ public class preprocesamiento {
     }
 
     private void añadirFiltrosCaracteres() {
-        gestorCaracters.add(new Filtro("\\p{Punct}", " "));
+       gestorCaracters.add(new Filtro("\\p{Punct}", " "));
+       //gestorCaracters.add(new Filtro("[^\\p{ASCII}]", " "));
         //eliminamos los numeros
         gestorCaracters.add(new Filtro("[^A-Za-z]", " "));
         //eliminamos  los "-" que no sean guiones
         gestorCaracters.add(new Filtro("-+ | -+", " "));
         //eliminamos los espacios duplicdos
         gestorCaracters.add(new Filtro(" +", " "));
+
         gestorCaracters.add(new Filtro("^\\s*", ""));
         gestorCaracters.add(new Filtro("\\s*$", ""));
+
     }
 
     private ArrayList<String> eliminarTerminos(ArrayList<String> asTerm) throws IOException, URISyntaxException {
